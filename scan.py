64c65a4006b7f692c30f6facd786853992395d75
masayoshikuroda <kuroda.masayoshi@gmail.com>
2025-05-01 15:29:13 +0900

    Fix: ignore argument

--- scan.py
+++ scan.py
@@ -5,6 +5,8 @@ from bleak import BleakScanner
 TARGET = 'YAS-108_BLE'
 if len(sys.argv) > 1:
   name = sys.argv[1]
+else:
+  name = TARGET
 
 async def run():
     scanner = BleakScanner()
@@ -17,10 +19,11 @@ async def run():
         device = results[id][0]
         data   = results[id][1]
  
-        if device.name == TARGET:
+        if device.name == name:
             print("---", id, '---')
             print("LOCAL_NAME : ", data.local_name)
             print("RSSI       : ", data.rssi)
 
-loop = asyncio.get_event_loop()
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
 loop.run_until_complete(run())
